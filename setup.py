d78edb17279e74945b35d2b542815ec6e0fe3da1
Arnab Mac <arnabborah@Arnabs-Mac-mini.local>
2020-09-22 11:50:11 -0700

    created python package

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='fling',
     packages=find_packages(include=['fling']),
-    version='0.1.1.0',
-    description='Text Clustering using DBSCAN and word vectors',
+    version='0.0.1',
+    description='fastLinguistics : unsupervised computational linguistics',
     author='Arnab Borah',
     license='MIT',
     long_description=long_description,
